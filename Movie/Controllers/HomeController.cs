2cce64e4ece84b57f58808f2a357dff350ced647
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-04-12 21:52:44 +0600

    Movies

---
+++ Movie/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Movie.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Movie.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        readonly string apiKey = "5b9b7798";
+        readonly string baseUrl = "https://omdbapi.com/";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Search(string title)
+        {
+            MovieApiResponse movies = null;
+            if (title != null)
+            {
+                HttpClient httpClient = new HttpClient();
+                var result = await httpClient.GetStringAsync($"{baseUrl}?s={title}&apikey={apiKey}");
+                movies = JsonConvert.DeserializeObject<MovieApiResponse>(result);
+                ViewBag.SearchTitle = title;
+                ViewBag.Result = result;
+            }
+           
+            return View(movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Movie/Models/MovieApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.Models
+{
+    public class MovieApiResponse
+    {
+        public Search[] Search { get; set; }
+        public string totalResults { get; set; }
+        public string Response { get; set; }
+    }
+}
